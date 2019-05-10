70dd22f7df4f4b85820506cb68847d14159d90e7
kevin schuchard <schuchard.kevin@gmail.com>
2019-05-10 09:13:37 -0600

    add @schuchard/prettier

--- src/app/core/from-mobx.service.ts
+++ src/app/core/from-mobx.service.ts
@@ -8,10 +8,10 @@ import { Observable } from 'rxjs';
 })
 export class FromMobxService {
   constructor() {}
-fromMobx<T>(expression: () => T, invokeImmediately: boolean = true): Observable<T> {
-    return new Observable((observer) => {
+  fromMobx<T>(expression: () => T, invokeImmediately: boolean = true): Observable<T> {
+    return new Observable(observer => {
       const computedValue = computed(expression);
-      const disposer = computedValue.observe((changes) => {
+      const disposer = computedValue.observe(changes => {
         observer.next(changes.newValue);
       }, invokeImmediately);
 
